2d794f7fa795784b577dba79a1996c9355d568d0
Chandler <chandler@multisiglabs.org>
2024-08-27 21:50:00 +0200

    Get initial mvp working!

--- watchdog/main.ts
+++ watchdog/main.ts
@@ -26,55 +26,76 @@ const getAllSubnets = async () => {
   return data;
 }
 
+// deno-lint-ignore no-explicit-any
+const onChange = async (_: any) => {
+  console.log('Subnet inserted or deleted');
+  const subnets = await getAllSubnets();
+  const config = {
+    'track-subnets': subnets.map((subnet) => subnet.subnet_id).join(',')
+  } as Config;
+
+  // read the existing config and include all existing attributes
+  try {
+    const decoder = new TextDecoder('utf-8');
+    const data = await Deno.readFile('/avalanche/configs/node.json');
+    const existingConfig: Config = JSON.parse(decoder.decode(data));
+    console.log('Existing config:', existingConfig);
+    config['public-ip'] = existingConfig['public-ip'];
+    config['http-host'] = existingConfig['http-host'];
+    console.log('New config:', config);
+  } catch (e) {
+    // ignore the error if the file does not exist
+    console.error(e);
+  }
+
+  // overwrite the config at avalanche/config.json
+  const encoder = new TextEncoder();
+  await Deno.writeFile('/avalanche/configs/node.json', encoder.encode(JSON.stringify(config)));
+  // make sure all the subnet VMs are in the plugins folder
+  // if they are not, copy subnet-evm to their respective VM ID
+  const vmIDs = subnets.map((subnet) => subnet.vm_id);
+  // list all files in the plugins folder
+  const files = Array.from(Deno.readDirSync('/avalanche/plugins'));
+  for (const vmID of vmIDs) {
+    // check if the file exists
+    const file = files.find((file) => file.name === vmID);
+    if (!file) {
+      // copy the file
+      Deno.copyFile('/avalanche/vms/subnet-evm', `/avalanche/plugins/${vmID}`);
+    }
+  }
+  // now check to make sure there are no extra files in the plugins folder
+  for (const file of files) {
+    if (!vmIDs.includes(file.name)) {
+      // delete the file
+      Deno.remove(`/avalanche/plugins/${file.name}`);
+    }
+  }
+  // list all containers
+  const containers = await docker.containers.list();
+  console.log('Containers:', containers);
+  // find the container id with the name "avalanche"
+  const container = containers.find((container) => container?.Names?.find((name) => name.includes('avalanche')));
+  if (container) {
+    // restart the container
+    await docker.containers.restart(container.Id!);
+  } else {
+    console.error('Container not found');
+  }
+}
+
 const changes = supabase
   .channel('table-db-changes')
   .on('postgres_changes', {
     event: 'INSERT',
     schema: 'public',
     table: 'subnets',
-  }, async (_) => {
-    const subnets = await getAllSubnets();
-    const config = {
-      'track-subnets': subnets.map((subnet) => subnet.subnet_id).join(',')
-    } as Config;
-
-    // read the existing config and include all existing attributes
-    try {
-      const decoder = new TextDecoder('utf-8');
-      const data = await Deno.readFile('./avalanche/config.json');
-      const existingConfig: Config = JSON.parse(decoder.decode(data));
-      config['public-ip'] = existingConfig['public-ip'];
-      config['http-host'] = existingConfig['http-host'];
-      Object.assign(config, existingConfig);
-    } catch (_) {
-      // ignore the error if the file does not exist
-    }
-
-    // overwrite the config at avalanche/config.json
-    const encoder = new TextEncoder();
-    await Deno.writeFile('./avalanche/config.json', encoder.encode(JSON.stringify(config)));
-    // make sure all the subnet VMs are in the plugins folder
-    // if they are not, copy srEXiWaHuhNyGwPUi444Tu47ZEDwxTWrbQiuD7FmgSAQ6X7Dy to their respective VM ID
-    const vmIDs = subnets.map((subnet) => subnet.vm_id);
-    // list all files in the plugins folder
-    const files = Array.from(Deno.readDirSync('./avalanche/plugins'));
-    for (const vmID of vmIDs) {
-      // check if the file exists
-      const file = files.find((file) => file.name === vmID);
-      if (!file) {
-        // copy the file
-        Deno.copyFile('./avalanche/plugins/srEXiWaHuhNyGwPUi444Tu47ZEDwxTWrbQiuD7FmgSAQ6X7Dy', `./avalanche/plugins/${vmID}`);
-      }
-    }
-    // list all containers
-    const containers = await docker.containers.list();
-    // find the container id with the name "avalanche"
-    const container = containers.find((container) => container?.Names?.includes('/avalanche'));
-    if (container) {
-      // restart the container
-      await docker.containers.restart(container.Id!);
-    }
-  });
+  }, onChange)
+  .on('postgres_changes', {
+    event: 'DELETE',
+    schema: 'public',
+    table: 'subnets',
+  }, onChange);
 
 changes.subscribe((status) => {
   if (status === 'SUBSCRIBED') {
